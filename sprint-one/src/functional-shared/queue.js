71aaf50024af73870e69e3df1037cc4d1b2617c1
JonathanEleiott <jonathan.eleiott@gmail.com>
2016-12-14 15:01:29 -0800

    pass first test of functional shared queue

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,18 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var sharedMethods = {};
+  
+  sharedMethods.howMany = 0;
+
+
+  _.extend(sharedMethods, queueMethods);
+  return sharedMethods;
 };
 
 var queueMethods = {};
 
-
+queueMethods.size = function () {
+  // body...
+  return this.howMany;
+};
