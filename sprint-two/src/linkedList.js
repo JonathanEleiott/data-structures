992b29b2de1cb363ce51a67fbbe7a9313306b30b
JonathanEleiott <jonathan.eleiott@gmail.com>
2016-12-15 11:02:38 -0800

    pass all of the original test for linkedList

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -2,17 +2,37 @@ var LinkedList = function() {
   var list = {};
   list.head = null;
   list.tail = null;
-  var storage = {};
 
   list.addToTail = function(value) {
-    var temp = Node(value);
-    list.tail = temp;
+    var newNode = Node(value);
+    if (list.tail === null) {
+      list.tail = newNode;
+      list.head = newNode;
+    } else {
+      list.tail.next = newNode;
+      list.tail = newNode;
+    }
   };
 
   list.removeHead = function() {
+    var temp = list.head.next;
+    var oldHead = list.head.value;
+    delete list.head;
+    list.head = temp;
+    return oldHead;
   };
 
-  list.contains = function(target) {
+  list.contains = function(target, node) {
+    node = node || list.head;
+    if (node.value === target) {
+      return true;
+    } else if (node.next === null) {
+      return false;     
+    } else {
+      return list.contains(target, node.next);
+    }
+    
+
   };
 
   return list;
@@ -27,7 +47,11 @@ var Node = function(value) {
   return node;
 };
 
-
+// linkedList.addToTail(4);
+// linkedList.addToTail(5);
+// expect(linkedList.head.value).to.equal(4);
+// linkedList.removeHead();
+// expect(linkedList.head.value).to.equal(5);
 
 /*
  * Complexity: What is the time complexity of the above functions?
