6002af3a4bfb28c153ff384336c6527ca8ea8ec9
JonathanEleiott <jonathan.eleiott@gmail.com>
2016-12-14 19:46:07 -0800

    Pass first three tests of pseudoclassical queue

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,23 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.storage = {};
+  this.length = 0;
+  this.head = 0;
+
 };
 
+Queue.prototype.enqueue = function(value) {
+  // body...
+  this.storage[length] = value;
+  this.length++;
+};
+
+Queue.prototype.dequeue = function() {
+
+};
+
+Queue.prototype.size = function() {
+  return this.length;
+};
 
