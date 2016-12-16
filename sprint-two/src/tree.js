1b51265b27f73c42fad42365ffde2f03ae9c420a
JonathanEleiott <jonathan.eleiott@gmail.com>
2016-12-15 16:19:21 -0800

    Pass all tests for tree

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -4,7 +4,7 @@ var Tree = function(value) {
   _.extend(newTree, treeMethods);
 
   // your code here
-  newTree.children = null;  // fix me
+  newTree.children = [];
 
   return newTree;
 };
@@ -12,13 +12,31 @@ var Tree = function(value) {
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  var node = Tree(value);
+  this.children[this.children.length] = node;  
+
 };
 
 treeMethods.contains = function(target) {
+  if (this.value === target) {
+    return true;
+  } else if (this.children.length !== 0) {
+    for (var i = 0; i < this.children.length; i++) {
+      if (this.children[i].contains(target)) {
+        return true;
+      }
+    }
+  }
+  return false;
 };
 
 
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ addChild runs in constant because we know the exact place where we want to add the node so assigning the node will always take the same amount of time
+ contains runs in linear time because in worst case scenario we have to check every node once
  */
+ 
+// tree.addChild(5);
+// expect(tree.contains(5)).to.equal(true);
