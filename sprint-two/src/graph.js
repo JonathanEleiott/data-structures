538eaf338991ef60c7735437103375af39dd5454
JonathanEleiott <jonathan.eleiott@gmail.com>
2016-12-16 10:34:58 -0800

    Pass all tests for graph

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -23,6 +23,11 @@ Graph.prototype.contains = function(node) {
 // Removes a node from the graph.
 Graph.prototype.removeNode = function(node) {
   delete this.nodes[node];
+  for (var key in this.nodes) {
+    if (this.nodes[key].indexOf(node) > -1) {
+      this.nodes[key][this.nodes[key].indexOf(node)] = null;
+    }
+  }
 };
 
 // Returns a boolean indicating whether two specified nodes are connected.  Pass in the values contained in each of the two nodes.
@@ -41,22 +46,37 @@ Graph.prototype.addEdge = function(fromNode, toNode) {
 
 // Remove an edge between any two specified (by value) nodes.
 Graph.prototype.removeEdge = function(fromNode, toNode) {
-  this.nodes[fromNode].indexOf(toNode) = null;
-  this.nodes[toNode].indexOf(fromNode) = null;
+  var index = this.nodes[fromNode].indexOf(toNode);
+  this.nodes[fromNode][index] = null;
+  index = this.nodes[toNode].indexOf(fromNode);
+  this.nodes[toNode][index] = null;
 };
 
 // Pass in a callback which will be executed on each node of the graph.
 Graph.prototype.forEachNode = function(cb) {
+  var result, key;
+  for (key in this.nodes) {
+    result = cb(key);
+
+  }
+  return result;
 };
 
 /*
  * Complexity: What is the time complexity of the above functions?
  */
 
+// var connectToFive = function(item) {
+//   graph.addEdge(item, 5);
+// };
 
-// graph.addNode(4);
 // graph.addNode(5);
-// graph.addEdge(5, 4);
-// expect(graph.hasEdge(4, 5)).to.equal(true);
-// graph.removeEdge(5, 4);
-// expect(graph.hasEdge(4, 5)).to.equal(false);
+// graph.addNode(2);
+// graph.addNode(1);
+// graph.addNode(3);
+// graph.forEachNode(connectToFive);
+// expect(graph.hasEdge(2, 5)).to.equal(true);
+// expect(graph.hasEdge(1, 5)).to.equal(true);
+// expect(graph.hasEdge(3, 5)).to.equal(true);
+// expect(graph.hasEdge(5, 5)).to.equal(true);
+
