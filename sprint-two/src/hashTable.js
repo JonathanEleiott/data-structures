50e2689d826703f25ef95f83015b73bbae100c05
JonathanEleiott <jonathan.eleiott@gmail.com>
2016-12-17 17:45:11 -0800

    Implement solution for comparison later

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -3,47 +3,30 @@
 var HashTable = function() {
   this._limit = 8;
   this._storage = LimitedArray(this._limit);
-  this._storage.newArray = [];
   this.counter = 0;
 };
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit, 'for insert', this.counter, this._limit);
-  var oldBucket = this._storage.get(index);
-  var tuple = [k, v];
-  var bucket = [];
-  bucket.push(tuple);
+  var bucket = this._storage.get(index) || [];
+
+
   
-  if (this.counter === this._limit * 0.75) {  //this._limit * 0.75 = 6
-    if (this._limit !== 16) {
-      this._limit *= 2;
-      this.resize(this._limit);
-      index = getIndexBelowMaxForKey(k, this._limit, 'for migration in insert');
-      oldBucket = this._storage.get(index);
+  for (var i = 0; i < bucket.length; i++) {
+    var tuple = bucket[i];  
+    if (tuple[0] === k) {
+      tuple[1] = v;
     }
   }
-
+   
+  bucket.push([k, v]);
+  this._storage.set(index, bucket);
   this.counter++;
+ 
 
-  if (oldBucket !== undefined) {
-    for (var i = 0; i < oldBucket.length; i++) {
-      if (oldBucket[i][0] === k) {
-        this._storage.set(index, bucket);
-        return; 
-      }
-    } 
-    oldBucket.push(tuple);
-    this._storage.set(index, oldBucket);
+  if (this.counter > this._limit * 0.75) {  //this._limit * 0.75 = 6
+    this.resize(this._limit * 2);
   }
-  //if storage does not have a array at the index
-  if (oldBucket === undefined) {
-    //set storage at index to equal new array with array with key and value
-    this._storage.set(index, bucket);
-  } 
-  //else if there is a array
-    //get the corrent value of the storage at that index
-    //set the new value of the storage at that index to an array with a previous array + new array
-
 };
 
 HashTable.prototype.retrieve = function(k) {
@@ -53,6 +36,7 @@ HashTable.prototype.retrieve = function(k) {
   if (bucket === undefined) {
     bucket = [];
   }
+
   //console.log('bucket', bucket);
   for (var i = 0; i < bucket.length; i++) {
     if (bucket[i][0] === k) {
@@ -63,48 +47,37 @@ HashTable.prototype.retrieve = function(k) {
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit, 'for remove', this.counter);
-  var oldBucket = this._storage.get(index);
+  var bucket = this._storage.get(index) || [];
 
-  if (this.counter < this._limit * 0.25) {
-    if (this._limit !== 8) {
-      this._limit *= .5;
-      this.resize(this._limit);
-      index = getIndexBelowMaxForKey(k, this._limit, 'for migration in remove', this.counter);
-      oldBucket = this._storage.get(index);
-    }
-  }
-
-  this.counter--;
-
-  //console.log(oldBucket);
-  for (var i = 0; i < oldBucket.length; i++) {
-    if (oldBucket[i][0] === k) {
-      oldBucket[i] = [];
+  for (var i = 0; i < bucket.length; i++) {
+    if (bucket[i][0] === k) {
+      bucket.splice(i, 1);
+      this.counter--;
+      if (this.counter < this._limit * 0.25) {
+        this.resize(Math.floor(this._limit / 2));
+      }
     }
   }
 };
 
 HashTable.prototype.resize = function(newLimit) {
   //resize this._storage
-  this.counter = 0;
-  if (newLimit < 8) {
-    return;
-  } else {
-    var oldStorage = this._storage;
-    this._storage = LimitedArray(newLimit);
-  }
-  
-  var callback = function(bucket, i, storage) {
-    if (bucket !== undefined) {
-      var context = this;
-      bucket.forEach(function(tuple) {
-        context.insert(tuple[0], tuple[1]);
-      });
-    }
-  };
+  var oldStorage = this._storage;
 
-  //migration
-  oldStorage.each(callback.bind(this));
+  newLimit = Math.max(newLimit, 8);
+  if (newLimit === this._limit) { return; }
+
+  this._limit = newLimit;
+  this._storage = LimitedArray(this._limit);
+  this.counter = 0;
+  
+  oldStorage.each(function(bucket) {
+    if (bucket === undefined) { return; }
+    for (var i = 0; i < bucket.length; i++) {
+      var tuple = bucket[i];
+      this.insert(tuple[0], tuple[1]);
+    }
+  }.bind(this));
   
 };
 
